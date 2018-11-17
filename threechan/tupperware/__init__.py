bf92f1a0127d598533da5e9af283e16f7484e2f3
Gideon Wald <gideon.wald@gmail.com>
2018-11-16 19:21:31 -0800

    oops

--- threechan/tupperware/__init__.py
+++ threechan/tupperware/__init__.py
@@ -25,7 +25,7 @@ andrea_cell = '+14155496087'
 lexi_cell = '+14088887272'
 #miju_cell = '+14152655604'
 peter_cell = '+14155099881'
-app_admins = [gideon_cell, andrea_cell, lexi_cell, miju_cell, peter_cell]
+app_admins = [gideon_cell, andrea_cell, lexi_cell, peter_cell]
 
 # Allocations - perhaps pre-loaded, perhaps not
 participant_number_allocations = {}
