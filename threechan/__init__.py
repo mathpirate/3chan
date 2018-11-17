7b8458d8871778b962438a75998ec8597b8f3473
Gideon Wald <gideon.wald@gmail.com>
2018-11-16 19:59:54 -0800

    gearing up for postgres

--- threechan/__init__.py
+++ threechan/__init__.py
@@ -1,6 +1,27 @@
 from flask import Flask
+from flask_heroku import Heroku
+from flask_sqlalchemy import SQLAlchemy
 from threechan.tupperware import flask_blueprint
 
 app = Flask(__name__)
-
 app.register_blueprint(flask_blueprint, url_prefix='/')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/threechan'
+heroku = Heroku(app)
+db = SQLAlchemy(app)
+
+class Allocation(db.Model):
+    __tablename__ = "allocations"
+    id = db.Column(db.Integer, primary_key=True)
+    participant_number = db.Column(db.String)
+    front_number = db.Column(db.String)
+    admin_number = db.Column(db.String)
+
+    def __init__(self, p, f, a):
+        self.participant_number = p
+        self.front_number = f
+        self.admin_number = a
+
+    def __repr__(self):
+        return 'P: %s, F: %s, A: %s' % (self.participant_number, self.front_number, self.admin_number)
+
+
